7ecb3f3bc94b04de571f21fca7e95dd8745d1e1b
Mohit <mohitparmar1501@gmail.com>
2024-05-16 11:08:53 +0530

    Secondary  Container with movie List && usePopularMovie hook

---
+++ src/components/MovieCard.js
@@ -0,0 +1,15 @@
+import { IMG_CDN_URL } from "../utils/constants"
+
+
+const MovieCard = ({poster_path}  ) => {
+  return (
+    <div className="w-48 pr-4">
+        <img alt="Movie Card" 
+          src= {IMG_CDN_URL + 
+            poster_path
+            } />
+    </div>
+  )
+}
+
+export default MovieCard
